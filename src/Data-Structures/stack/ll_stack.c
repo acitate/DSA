20aba37539c3d7d02f7da5e74e1044b70be6807e
acitate <m.parsa.kolivand@gmail.com>
2026-01-07 07:37:12 +0330

    Add documentation comments to ll_stack.c

--- src/Data-Structures/stack/ll_stack.c
+++ src/Data-Structures/stack/ll_stack.c
@@ -4,6 +4,12 @@
 #include <linked_list.h>
 
 
+/*
+ * Create a linked list based stack with the given data.
+ *
+ * @param data Pointer to element to store in the stack
+ * @return Pointer to the newly created stack
+ */
 LLStack *stack_ll_create(void *data) {
     LLStack *new_stack = (LLStack*) malloc(sizeof(LLStack));
 
@@ -13,6 +19,13 @@ LLStack *stack_ll_create(void *data) {
 }
 
 
+/*
+ * Push a new element to top of the stack.
+ *
+ * @param stack Pointer to pointer of the stack that is to be pushed to
+ * @param data Pointer to data to push
+ * @return 0 on success, 1 on fail.
+ */
 int stack_ll_push(LLStack **stack, void *data) {
     if (stack == NULL || *stack == NULL) {
         fprintf(stderr ,"Stack is NULL!\n");
@@ -28,6 +41,13 @@ int stack_ll_push(LLStack **stack, void *data) {
 }
 
 
+/*
+ * View the top element of the stack without removing it.
+ *
+ * @param stack Pointer to the stack to peek
+ * @param out Pointer to pointer of the memory to store peek'd element
+ * @return 0 on success, 1 on fail
+ */
 int stack_ll_peek(LLStack *stack, void **out) {
     if (stack == NULL) {
         fprintf(stderr, "Stack is NULL!\n");
@@ -44,9 +64,17 @@ int stack_ll_peek(LLStack *stack, void **out) {
         return 1;
     } 
     *out = node->data;
+    return 0;
 } 
 
 
+/*
+ * Pop (remove and return) the element at top of the stack.
+ *
+ * @param stack Pointer to pointer of the stack to pop
+ * @param out Pointer to pointer of the variable to store the popped element
+ * @return 0 on success, 1 on fail
+ */
 int stack_ll_pop(LLStack **stack, void **out) {
 
     if (stack == NULL || *stack == NULL) {
@@ -66,6 +94,12 @@ int stack_ll_pop(LLStack **stack, void **out) {
 }
 
 
+/*
+ * Get the number of elements stored in a stack.
+ *
+ * @param stack Pointer to stack to get size of
+ * @return Number of elements in the stack 
+ */
 int stack_ll_size(LLStack *stack) {
     return ll_length(stack->list);
 }
